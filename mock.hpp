7a93449eacf3cd584f38cccdc20a66b4e1054ebd
Atila Neves <atila.neves@gmail.com>
2016-03-23 00:03:49 +0100

    Function traits

--- mock.hpp
+++ mock.hpp
@@ -8,34 +8,13 @@
 
 
 template<typename F>
-struct StdFunctionTypeImpl;
+struct FunctionTraits;
 
 template<typename R, typename... A>
-struct StdFunctionTypeImpl<R(*)(A...)> {
-    using Type = std::function<R(A...)>;
+struct FunctionTraits<R(*)(A...)> {
+    using StdFunctionType = std::function<R(A...)>;
+    using ReturnType = R;
 };
-
-/**
- Alias for the std::function type corresponding to the function pointer
- type F, e.g.
-
- int foo(int, float);
- static_assert(std::is_same<StdFunctionType<decltype(&foo)>, std::function<int(int, float>>);
- */
-template<typename F>
-using StdFunctionType = typename StdFunctionTypeImpl<F>::Type;
-
-template <typename F>
-struct ReturnTypeImpl;
-
-template<typename R, typename... A>
-struct ReturnTypeImpl<R(*)(A...)> {
-    using Type = R;
-};
-
-template<typename T>
-using ReturnType = typename ReturnTypeImpl<T>::Type;
-
 
 
 /**
@@ -54,7 +33,7 @@ using ReturnType = typename ReturnTypeImpl<T>::Type;
  int foo(int, float);
  extern std::function<int(int, float)> mock_foo;
  */
-#define DECL_MOCK(func) extern StdFunctionType<decltype(&func)> mock_##func
+#define DECL_MOCK(func) extern FunctionTraits<decltype(&func)>::StdFunctionType mock_##func
 
 /**
  Definition of the std::function that will store the implementation.
@@ -69,26 +48,26 @@ using ReturnType = typename ReturnTypeImpl<T>::Type;
 
 #define IMPL_MOCK_0(func) \
     MOCK_STORAGE(func); \
-    extern "C" ReturnType<decltype(&func)> ut_##func() { \
+    extern "C" FunctionTraits<decltype(&func)>::ReturnType ut_##func() {  \
         return mock_##func(); \
     }
 
 #define IMPL_MOCK_1(func, T0) \
     MOCK_STORAGE(func); \
-    extern "C" ReturnType<decltype(&func)> ut_##func(T0 UT_ARG(0)) { \
+    extern "C" FunctionTraits<decltype(&func)>::ReturnType ut_##func(T0 UT_ARG(0)) { \
         return mock_##func(UT_ARG(0)); \
     }
 
 #define IMPL_MOCK_2(func, T0, T1) \
     MOCK_STORAGE(func); \
-    extern "C" ReturnType<decltype(&func)> ut_##func(T0 UT_ARG(0), T1 UT_ARG(1)) { \
+    extern "C" FunctionTraits<decltype(&func)>::ReturnType ut_##func(T0 UT_ARG(0), T1 UT_ARG(1)) { \
         return mock_##func(UT_ARG(0), UT_ARG(1)); \
     }
 
 
 #define IMPL_MOCK_4(func, T0, T1, T2, T3)    \
     MOCK_STORAGE(func); \
-    extern "C" ReturnType<decltype(&func)> ut_##func(T0 a0, T1 a1, T2 a2, T3 a3) { \
+    extern "C" FunctionTraits<decltype(&func)>::ReturnType ut_##func(T0 a0, T1 a1, T2 a2, T3 a3) { \
         return mock_##func(a0, a1, a2, a3); \
     }
 
