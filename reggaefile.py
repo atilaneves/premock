b4ce0b348df57eb19f16fa5beb17352bd07b54a3
Atila Neves <atila.neves@gmail.com>
2016-03-22 20:33:07 +0100

    Recuperated Makefile functionality

--- reggaefile.py
+++ reggaefile.py
@@ -1,5 +1,10 @@
 from reggae import *
 
-objs = object_files(src_dirs=["."], flags='-Wall -Werror -std=c++14 -g')
-test = link(exe_name="test", dependencies=objs)
+
+flags = '-Wall -Werror -std=c++14 -g'
+prod_files = ['prod.cpp']
+objs = object_files(src_dirs=["."], flags=flags, exclude_files=prod_files)
+prod_objs = object_files(src_files=prod_files,
+                         flags=flags + ' -include mock.hpp')
+test = link(exe_name="test", dependencies=[objs, prod_objs])
 build = Build(test)
--- test.cpp
+++ test.cpp
@@ -12,6 +12,6 @@ int main() {
     // if(argc != 2) throw runtime_error("Must pass in int");
     // const auto i = stoi(argv[1]);
     const auto res = func(42);
-    if(res != 7) throw runtime_error("Must be 7!!!!!");
+    if(res != 7) throw runtime_error("Unexpected value: " + to_string(res));
     cout << "Result was " << res << endl;
 }
