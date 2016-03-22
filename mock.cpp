e89f413ba851a036b605a451ae249958ffe078e3
Atila Neves <atila.neves@gmail.com>
2016-03-22 20:44:10 +0100

    Better

--- mock.cpp
+++ mock.cpp
@@ -1,10 +1,11 @@
 #include "mock.hpp"
+#undef send
 #include <functional>
 
 using namespace std;
 
 
-function<ssize_t(int, const void *, size_t, int)> mock_send;
+function<ssize_t(int, const void *, size_t, int)> mock_send = send;
 
 ssize_t ut_send(int socket, const void *buffer, size_t length, int flags) {
     return mock_send(socket, buffer, length, flags);
--- prod.cpp
+++ prod.cpp
@@ -1,7 +1,6 @@
 #include <sys/socket.h>
 
 
-
 int func(int fd) {
     const void* buffer{};
     const size_t length{};
