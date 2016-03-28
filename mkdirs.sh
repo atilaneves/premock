11cc1a54273326bedb1da1c5ba935490ab9941c7
Atila Neves <atila.neves@gmail.com>
2016-03-28 15:54:04 +0200

    Another attempt at fixing the Travis build

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,4 +1,4 @@
 set -e
 mkdir -p objs/example_cpp.objs/example/{cpp,d,deps,src}
 mkdir -p objs/example_cpp.objs/example/cpp/{mocks,test}
-mkdir -p objs/ut.objs/tests
+mkdir -p objs/ut_cpp.objs/tests
