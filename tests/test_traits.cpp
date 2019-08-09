71be044e5a0894b291da561cdae40762691c298d
Josko <josko.nikolic1@gmail.com>
2019-08-09 12:42:01 +0200

    Simplify the argument type deduction. This also enables proper mocking of functions with lvalue/rvalue references as parameters.

--- tests/test_traits.cpp
+++ tests/test_traits.cpp
@@ -11,11 +11,11 @@ TEST_CASE("StdFunctionTraits TupleType") {
 }
 
 
-int func(string, double, const char*);
+int func(string, double, const char*, int&, string&&);
 
 TEST_CASE("FunctionTraits std::function type") {
     static_assert(is_same<FunctionTraits<decltype(&func)>::StdFunctionType,
-                          function<int(string, double, const char*)>>::value, "Wrong std::function type");
+                          function<int(string, double, const char*, int&, string&&)>>::value, "Wrong std::function type");
 }
 
 
@@ -34,4 +34,10 @@ TEST_CASE("FunctionTraits parameter type") {
 
     static_assert(is_same<FunctionTraits<decltype(&func)>::Arg<2>::Type,
                           const char*>::value, "Wrong parameter type");
+                          
+    static_assert(is_same<FunctionTraits<decltype(&func)>::Arg<3>::Type,
+                          int&>::value, "Wrong parameter type");
+
+    static_assert(is_same<FunctionTraits<decltype(&func)>::Arg<4>::Type,
+                          string&&>::value, "Wrong parameter type");
 }
